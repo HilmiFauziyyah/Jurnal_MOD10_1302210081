a529d687a1783c4c0bdfd638e6053981d65b4253
HilmiFauziyyah <hilmifs2113@gmail.com>
2023-05-03 17:13:43 +0700

    MatematikaLibraries

---
+++ Jurnal_MOD10_1302210081/1302210081/Program.cs
@@ -0,0 +1,10 @@
+﻿using MatematikaLibraries;
+
+string jawaban = MatematikaLibraries.Class1.turunan(new int[] { 1, 4, -12, 9 });
+string jawaban2 = MatematikaLibraries.Class1.integral(new int[] {4,5,-12,9});
+
+Console.WriteLine($"hasil  dari 4x^3 + 6x^2-12x +9 yaitu = \n" +
+     $"turunan = 3x2 +8x - 12\n"+
+     $"integral = x4 + 2x3 - 6x2 +9x+C"
+    );
+
---
+++ Jurnal_MOD10_1302210081/MatematikaLibraries/Class1.cs
@@ -0,0 +1,86 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace MatematikaLibraries
+{
+    public class Class1
+    {
+        public void FPB(int temp, int input1, int input2)
+        {
+            while(input2 != 0)
+            {
+                temp = input2;
+                input2 = input1 % input1;
+                input1 = temp;
+            }
+        }
+        public void KPK(int result, int input1, int input2)
+        {
+
+            int max;
+            int min; 
+            max = Math.Max(input1, input2);
+            min = Math.Min(input1, input2);
+            result = max;
+
+            while(result % min != 0)
+            {
+                result = result + max;
+            }
+
+        }
+        public static string turunan(int[]persamaan)
+        {
+            int n;
+            int i;
+            string result;
+            n = persamaan.Length - 1;
+            int[] turunan = new int[n+1];
+
+            for (i = 1; i <= n; i++)
+            {
+                turunan[i] = persamaan[i] * (n - 1);
+            }
+
+            result = turunan[0] + "x^" + (n - 1);
+            
+            for (i = 1; i<n; i++)
+            {
+                if (turunan[i] > 0 )
+                {
+                    result = result + " + " + turunan[i] + "x" + (n - i - 1);
+                }else if(turunan[i] < 0){
+                    result = result+ "-" + (turunan) +"x"+(n -i -1);
+                }
+            }
+            return result;
+        }
+
+        public static string integral(int[] persamaan)
+        {
+            int n;
+            int i;
+            string result;
+            n = persamaan.Length;
+            int[] integral = new int[n+1];
+
+            for (i = 1; i <= n; i++)
+            {
+                integral[i] = persamaan[i - 1] / i;
+            }
+            result = "C";
+
+            for (i = n; i >= 0; i--)
+            {
+                if (integral[i] > 0)
+                {
+                    result = "+" + integral[i] + "x" + (i + 1) + result;
+                }else if (integral[i] < 0)
+                {
+                    result = integral[i] +"x^" + (i + 1) + result;
+                }
+            }
+            return result;
+        }
+        
+    }
+}
